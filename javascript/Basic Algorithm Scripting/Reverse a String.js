eb40e4a861f0ac851f18c88692bd1e58793f225b
Caio Amaral Corrêa <caiobep@me.com>
2016-12-29 15:42:53 -0200

    Basic Algorithm - Revert String

---
+++ javascript/Basic Algorithm Scripting/Reverse a String.js
@@ -0,0 +1,11 @@
+// Free Code Camp
+// Javascript module ==> Basic Scripting
+// Exercise ==> Reverse a String
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
+reverseString("Howdy");
+reverseString("Greatings from earth");
